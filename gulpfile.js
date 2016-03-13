42f7a3da5e71c8082cedfb36bed29d8f81b11e74
samelamin <hussam.elamin@gmail.com>
2016-03-13 20:45:01 +0000

    Refactor gulp file and added test task

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,17 @@ var shell = require('gulp-shell')
 gulp.task('default', ['restore','build']);
 
 gulp.task('restore', shell.task([
-  'dnu restore ./src/CoreConsoleApp ./src/CoreConsoleApp.Tests'
+  'dnu restore ./src/CoreConsoleApp*'
 ]));
 
 gulp.task('build', shell.task([
-  'dnu build ./src/CoreConsoleApp ./src/CoreConsoleApp.Tests'
+  'dnu build ./src/CoreConsoleApp*'
 ]));
+
+gulp.task('test', shell.task([
+  'dnx -p ./src/*.Tests/ test'
+]));
+
+gulp.task('watch', function () {
+  gulp.watch('*.cs', ['test']);
+});
